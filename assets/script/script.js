43960b253e158edc149852708bef83bb084cd793
wasep <wasepabdulaziz192@gmail.com>
2024-06-10 12:22:59 +0700

    Tambah anim

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,6 +1,25 @@
-const menuToggle = document.querySelector('.menu-toggle input');
-const nav = document.querySelector('nav ul');
+const menuToggle = document.querySelector(".menu-toggle input");
+const nav = document.querySelector("nav ul");
 
-menuToggle.addEventListener('click', function(){
-    nav.classList.toggle('slide');
+menuToggle.addEventListener("click", function () {
+  nav.classList.toggle("slide");
+});
+document.addEventListener("DOMContentLoaded", function () {
+  const observerOptions = {
+    threshold: 0.1,
+  };
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("animate");
+        observer.unobserve(entry.target);
+      }
+    });
+  }, observerOptions);
+
+  const elementsToAnimate = document.querySelectorAll(".zoom-in");
+  elementsToAnimate.forEach((element) => {
+    observer.observe(element);
+  });
 });
